090a5a37378082454ad9724a458a767889db6742
Michelle Hoelzer <hoelzermm@gmail.com>
2019-02-07 12:11:01 -0500

    first commit?

---
+++ main.py
@@ -0,0 +1,18 @@
+def proper_brackets(string):
+    for element in string:
+        if "(" in element:
+            return True
+
+
+print(proper_brackets("(*a++(*)"))
+print(proper_brackets("(*a{+}*)"))
+print(proper_brackets("    <************)>"))
+print(proper_brackets("    ()(***)(**)"))
+print(proper_brackets("   ()(***)(*)"))
+print(proper_brackets("({{}{}}[{(){}[]}"))
+print(proper_brackets("   ([))"))
+print(proper_brackets(" ()(**)"))
+print(proper_brackets("    ()*"))
+print(proper_brackets(" aaaaaaa"))
+print(proper_brackets("    aaa(aaaa"))
+print(proper_brackets(" *******"))
